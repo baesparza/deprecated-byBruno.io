077337ae9c4c9495ffbdd2dc53af1f7896fa72c4
Bruno Esparza <bruno.be81@gmail.com>
2021-11-03 17:03:15 -0500

    feat(router): add home page and resume pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,15 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { Route, Routes } from "react-router-dom";
+import { Home } from './home/home.page';
+import { Resume } from './resume/resume.page';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="bg-red-100">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            Counter is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="resume" element={<Resume />} />
+      </Routes>
+    </main>)
 }
 
 export default App
---
+++ src/resume/resume.page.tsx
@@ -0,0 +1,5 @@
+export function Resume() {
+    return (
+        <h1>Resume</h1>
+    );
+}
