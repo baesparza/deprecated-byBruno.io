1bdc2c43381c4208cbe0c6ea2e6ca97446869ce4
Bruno Esparza <bruno.be81@gmail.com>
2021-11-14 01:26:24 -0500

    hide home

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import { Home } from "./home/home.page";
 import { Resume } from './resume/resume.page';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      {/* <Route path="/" element={<Resume />} /> */}
+      {/* <Route path="/" element={<Home />} /> */}
+      <Route path="/" element={<Resume />} />
       <Route path="resume" element={<Resume />} />
     </Routes>
   )
