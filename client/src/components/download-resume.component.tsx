0dab3df52e97e3e1a380ce3015bec998cfa12daf
Bruno Esparza <bruno.be81@gmail.com>
2021-11-28 15:54:08 -0500

    fix: remove state on downloading pdg

--- client/src/components/download-resume.component.tsx
+++ client/src/components/download-resume.component.tsx
@@ -1,27 +1,15 @@
 import { useState } from 'react';
-import { FiCircle, FiDownload } from 'react-icons/fi';
+import { FiDownload } from 'react-icons/fi';
 
 
 export const DownloadResumeComponent: React.FC = () => {
-    const [downloading, setDownloading] = useState<boolean>(false)
-    console.log({ downloading })
     return (
         <a
             href='http://bybruno.io/functions/download-resume'
             download
-            onClick={() => setDownloading(true)}
             className=" flex gap-2 items-center text-sm font-medium bg-primary text-white rounded px-4 py-2 transform hover:-translate-y-1 transition-transform">
-            {
-                downloading
-                    ? <>
-                        <span>Downloading</span>
-                        <FiCircle ></FiCircle>
-                    </>
-                    : <>
-                        <span>Download</span>
-                        <FiDownload ></FiDownload>
-                    </>
-            }
+            <span>Download</span>
+            <FiDownload ></FiDownload>
         </a>
     );
 }
