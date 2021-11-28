b4139e2c907c0581f58f7d9d6bf4f215d12a1771
Bruno Esparza <bruno.be81@gmail.com>
2021-11-28 02:22:13 -0500

    feat(server): add generate pdf

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,22 @@
-FROM node:17-alpine3.12
-
+FROM node:14-alpine3.14
 
 # install typescript to use it accross all project
 RUN npm install typescript -g
+
+# Installs latest Chromium (92) package.
+RUN apk add --no-cache \
+      chromium \
+      nss \
+      freetype \
+      harfbuzz \
+      ca-certificates \
+      ttf-freefont \
+      nodejs \
+      yarn
+
+# Tell Puppeteer to skip installing Chrome. We'll be using the installed package.
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true \
+    PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium-browser
 
 # swap working directory
 # setup server dependecies
--- server/src/core/generate-resume-pdf.ts
+++ server/src/core/generate-resume-pdf.ts
@@ -3,12 +3,21 @@ import puppeteer from "puppeteer";
 const RESUME_URL = 'https://bybruno.io/resume';
 
 export const GenerateResumePdf = async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: true,
+        executablePath: '/usr/bin/chromium-browser',
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+        ],
+        ignoreHTTPSErrors: true
+    });
+
     const page = await browser.newPage();
-    await page.goto(RESUME_URL, { waitUntil: 'networkidle0' });
+    await page.goto(RESUME_URL, { waitUntil: 'networkidle2' });
 
     const pdf = await page.pdf({
-        printBackground: true,
         format: "a4",
         margin: {
             top: "20px",
