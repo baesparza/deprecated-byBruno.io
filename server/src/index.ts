ddb5d6ade31fb9e8ba265de4a0b245b82892ddd8
Bruno Esparza <bruno.be81@gmail.com>
2022-02-12 22:36:02 -0500

    add env file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,8 @@
+import { config as LoadEnvVariables } from "dotenv";
 import { App } from './app';
 
+/// load env
+LoadEnvVariables()
 const app = App({ logger: true });
 
 app.listen(process.env.PORT || 5000, "0.0.0.0", (err, address) => {
